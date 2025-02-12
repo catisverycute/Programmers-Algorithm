b56cc5eba6b067ea65faaffed7f95373dcc7b80f
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-02-12 23:23:59 +0900

    [level 0] Title: 코드 처리하기, Time: 11.57 ms, Memory: 39.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.js
@@ -0,0 +1,16 @@
+function solution(code) {
+    let mode = 0;
+    let ret = ""
+
+    for (let idx = 0; idx < code.length; idx++){
+        if(mode == 0){
+        code[idx] == '1' ? (mode = 1) : (idx % 2 == 0 && (ret += code[idx]))
+        }else{
+            code[idx] =='1' ? (mode = 0) : (idx % 2 !== 0 && (ret += code[idx]))
+        }
+  
+    }
+    
+    return ret.length > 0 ? ret : "EMPTY";
+  
+}
