c8c8b9a72e2f59564141fb6e2fa1c68c2cd4afbd
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-12-28 23:54:40 +0900

    [level 0] Title: 약수 구하기, Time: 0.45 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let arr =[]
+    for(divisor = 1; divisor <= n; divisor++){
+        if(n % divisor === 0){
+            arr.push(divisor)
+        }
+    }
+    return arr
+}
